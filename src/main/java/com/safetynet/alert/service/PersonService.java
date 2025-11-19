5e0df26ef68fbbfa050daa3f8ab9ad05dd293bcb
Septieme7 <lordstiko7@gmail.com>
2025-11-19 16:36:29 +0100

    Rectification et commentaires

--- src/main/java/com/safetynet/alert/service/PersonService.java
+++ src/main/java/com/safetynet/alert/service/PersonService.java
@@ -15,16 +15,22 @@ public class PersonService {
   public PersonService(PersonRepository personRepository) {
     this.personRepository = personRepository;
   }
-
+//Je veux
   public List<String> findAllEmailsByCity(String city) {
+    //La liste de
     List<String> personsMails = new ArrayList<> ();
     List<Person> persons = personRepository.findAll ();
+    //Boucle pour verif les persons qui sont dans la ville demander
     for (Person person : persons) {
+      //Et alors SI la person ce trouve dans la ville alors rajout dans la liste de leurs EMAILS
       if (person.getCity().equals(city)) {
-        personsMails.add (person.getEmail());
+        personsMails.add(person.getEmail());
       }
     }
+    //Voir le resultat
     return personsMails;
   }
+
+
 
 }
